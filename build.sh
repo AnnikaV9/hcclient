eeaf67e49ecfe18309dd88c8a1ad49245af9c0e7
carrot <68383195+AnnikaV9@users.noreply.github.com>
2023-10-18 20:49:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,28 +5,47 @@
 VERSION="1.3.3"
 
 binaries() {
+   for cmd in ldd objdump objcopy python3
+   do
+     if ! command -v $cmd &> /dev/null
+     then
+       echo "command '$cmd' not found"
+       exit 1
+     fi
+   done
+
    python3 -m venv .venv &&
    source .venv/bin/activate &&
 
-   pip install -r requirements.txt &&
-   pip install pyinstaller staticx &&
+   python3 -m pip install -r requirements.txt &&
+   python3 -m pip install pyinstaller staticx &&
 
    pyinstaller --onefile \
                --clean \
                --name hcclient-$VERSION \
                hcclient/__main__.py &&
+
    staticx --strip dist/hcclient-$VERSION dist/hcclient-$VERSION-static
 }
 
 container() {
-   if [ -x "$(command -v podman)" ]; then
-      podman build --tag hcclient . &&
-      podman save --output dist/hcclient-$VERSION-image.tar hcclient
-   elif [ -x "$(command -v docker)" ]; then
-      docker build --tag hcclient . &&
-      docker save --output dist/hcclient-$VERSION-image.tar hcclient
+   if ! command -v xz &> /dev/null
+   then
+     echo "command 'xz' not found"
+     exit 1
+   fi
+
+   if command -v podman &> /dev/null
+   then
+     podman build --tag hcclient . &&
+     podman save --output dist/hcclient-$VERSION-image.tar hcclient
+   elif command -v docker &> /dev/null
+   then
+     docker build --tag hcclient . &&
+     docker save --output dist/hcclient-$VERSION-image.tar hcclient
    else
-      echo "no container runtime found"; exit 1
+     echo "no container runtime found"
+     exit 1
    fi &&
 
    xz --compress \
@@ -39,8 +58,8 @@ container() {
 }
 
 case "$1" in
-    binaries) mkdir dist && binaries ;;
-    container) mkdir dist && container ;;
-    all) mkdir dist && binaries && container ;;
-    *) echo "commands: binaries, container, all"; exit 1  ;;
+  binaries) mkdir dist && binaries ;;
+  container) mkdir dist && container ;;
+  all) mkdir dist && binaries && container ;;
+  *) echo "commands: binaries, container, all"; exit 1  ;;
 esac
