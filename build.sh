b135cacd8f5144496341127d2dd188cea3ce7781
carrot <68383195+AnnikaV9@users.noreply.github.com>
2023-10-29 16:44:25 +0800

    1.10.3

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #
 # Requires ldd and binutils
 
-VERSION="1.10.2-git"
+VERSION="1.10.3-git"
 
 prepare() {
    if [[ "$RELEASE_VERSION" ]]
--- hcclient/__main__.py
+++ hcclient/__main__.py
@@ -2,7 +2,7 @@
 #
 # Author:    AnnikaV9
 # License:   Unlicense
-# Version:   1.10.2-git
+# Version:   1.10.3-git
 
 # import required modules
 import json
@@ -921,7 +921,7 @@ def main():
     optional_group.add_argument("--mod-nickname-color", help="sets the moderator nickname color (default: cyan)")
     optional_group.add_argument("--admin-nickname-color", help="sets the admin nickname color (default: red)")
     optional_group.add_argument("--proxy", help="specify a proxy to use (format: TYPE:HOST:PORT) (default: None)")
-    optional_group.add_argument("--version", help="displays the version and exits", action="version", version="hcclient 1.10.2-git")
+    optional_group.add_argument("--version", help="displays the version and exits", action="version", version="hcclient 1.10.3-git")
     optional_group.set_defaults(gen_config=False,
                                 config_file=None,
                                 no_config=False,
